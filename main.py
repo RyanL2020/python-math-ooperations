ee522e7eb722cb91b635866b325d855e47ad1bcd
Ryan Locascio <63062893+RyanL2020@users.noreply.github.com>
2022-01-31 15:47:20 +0000

    f-string

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
-# 🚨 Don't change the code above 👆
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# # 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
-new_height = int(height)
-new_weight = int(weight)
-# print(type(new_height))
-bmi = new_weight  / new_height ** 2
-print(int(bmi))
+# #Write your code below this line 👇
+# new_height = int(height)
+# new_weight = int(weight)
+# # print(type(new_height))
+# bmi = new_weight  / new_height ** 2
+# print(int(bmi))
+
+# F strings
+score = 0
+height = 1.8
+isWinning = True
+
+# f-string
+print(f"your score is {score} your height is {height}, you are winning is {isWinning}")
+
+
+
